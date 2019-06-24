5a686444e3bc766c4df34fbc71476012a0d33686
trainshogi <densyashogi@gmail.com>
2019-06-24 20:15:17 +0900

    先手側持ち駒修正

--- js/main.js
+++ js/main.js
@@ -25,6 +25,33 @@ function fix(string){
   fix_imglist.style.display = "none";
 }
 
+function fix_mochigoma(method,sengo,string){
+  var kazu = 1; // == real num
+  var koma = ["歩","香","桂","銀","金","角","飛"]
+  var kansuuji = ["","","二","三","四","五","六","七","八","九","十","十一","十二","十三","十四","十五","十六","十七","十八"];
+  var result_place = document.getElementById('board');
+  if(method=='add'){
+    if (sengo=="gote"){// gote
+      if (result_json['gote_mochi'][string]){
+        if (result_json['gote_mochi'][string] == ""){kazu == 2;}
+        else {kazu=(kansuuji.indexOf(result_json['gote_mochi'][string])+1)%19;}}
+      result_json['gote_mochi'][string] = kansuuji[kazu];
+
+    }
+    else{              // sente
+      var mochigoma = document.getElementsByClassName('mochigoma-list')[0].children[string.indexOf(koma)].children[0];
+      if (result_json['sente_mochi'][string]){
+        if (result_json['sente_mochi'][string] == ""){kazu == 2;}
+        else {kazu=(kansuuji.indexOf(result_json['sente_mochi'][string])+1)%19;}}
+      result_json['sente_mochi'][string] = kansuuji[kazu];
+      mochigoma.children('p')[0].data = String(kazu);
+      if (kazu == 0){mochigoma.children('img')[0].style.opacity=0.5;}
+      else {mochigoma.children('img')[0].style.opacity=1;}
+    }
+  }
+  result_place.textContent = json_to_kif(result_json);
+}
+
 function clip_text(){
   var result_txt = document.getElementById('board');
   if(execCopy(result_txt.value)){
